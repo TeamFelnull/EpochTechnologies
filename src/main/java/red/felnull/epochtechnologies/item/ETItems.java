0fa7d5a6f89ab682bca81c4b0b9242fbd6475760
okayu <siki6665@gmail.com>
2020-06-27 11:33:31 +0900

    aaaaaa
    aaaaa

--- src/main/java/red/felnull/epochtechnologies/item/ETItems.java
+++ src/main/java/red/felnull/epochtechnologies/item/ETItems.java
@@ -22,6 +22,28 @@ public class ETItems {
     //植物
     public static final Item COTTON = newItem("cotton");
     public static final Item TEA_LEAF = newItem("tea_leaf");
+    public static final Item papper = newItem("papper");
+    public static final Item ding = newItem("ding");
+    public static final Item cinammon = newItem("cinammon");
+    public static final Item natmeg = newItem("natmeg");
+    public static final Item cardamom = newItem("cardamon");
+    public static final Item ginger = newItem("ginger");
+
+    //穀物
+    public static final Item rice = newItem("rice");
+    public static final Item soy = newItem("soy");
+    public static final Item wheat = newItem("wheat");
+    public static final Item corn = newItem("corn");
+
+    //毛皮
+    public static final Item wolfhide = newItem("wolfhide");
+    public static final Item sheephide = newItem("sheephide");
+    public static final Item pighide = newItem("pighide");
+    public static final Item  = newItem("");
+    public static final Item  = newItem("");
+    public static final Item  = newItem("");
+
+
 
     public static void registerItem(IForgeRegistry<Item> r) {
         registryItem(r, TEST_ITEM);
@@ -37,6 +59,31 @@ public class ETItems {
 
         registryItem(r, COTTON);
         registryItem(r, TEA_LEAF);
+        registryItem(r, papper);
+        registryItem(r, ding);
+        registryItem(r, cinnamon);
+        registryItem(r, natmeg);
+        registryItem(r, cardamon);
+        registryItem(r, ginger);
+        registryItem(r, Opium);
+
+        registryItem(r,rice );
+        registryItem(r,soy );
+        registryItem(r,wheat );
+        registryItem(r,corn );
+
+        registryItem(r,wolfhide );
+        registryItem(r,sheephide );
+        registryItem(r, );
+        registryItem(r, );
+        registryItem(r, );
+        registryItem(r, );
+        registryItem(r, );
+        registryItem(r, );
+        registryItem(r, );
+        registryItem(r, );
+
+
 
     }
 
