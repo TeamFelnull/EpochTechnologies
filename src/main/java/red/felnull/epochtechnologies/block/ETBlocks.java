f77372028cfd8ffe7deb9ab8bab7b8daad87d555
MORIMORI <morimori.0317@outlook.jp>
2020-06-27 19:42:32 +0900

    クラッシュ修正

--- src/main/java/red/felnull/epochtechnologies/block/ETBlocks.java
+++ src/main/java/red/felnull/epochtechnologies/block/ETBlocks.java
@@ -20,6 +20,33 @@ public class ETBlocks {
     public static final Block SILVER_ORE = newOreBlock("silver_ore", 1, 3.0f, 3.0f);
     public static final Block RUBY_ORE = newOreBlock("ruby_ore", 1, 3.0f, 3.0f);
     public static final Block SAPPHIRE_ORE = newOreBlock("sapphire_ore", 1, 3.0f, 3.0f);
+    public static final Block COPPER_BLOCK = newOreBlock("copper_block", 1, 3.0f, 3.0f);
+    public static final Block TIN_BLOCK = newOreBlock("tin_block", 1, 3.0f, 3.0f);
+    public static final Block LEAD_BLOCK = newOreBlock("lead_block", 1, 3.0f, 3.0f);
+    public static final Block NICKEL_BLOCK = newOreBlock("nickel_block", 1, 3.0f, 3.0f);
+    public static final Block SILVER_BLOCK = newOreBlock("silver_block", 1, 3.0f, 3.0f);
+    public static final Block RUBY_BLOCK = newOreBlock("ruby_block", 1, 3.0f, 3.0f);
+    public static final Block SAPPHIRE_BLOCK = newOreBlock("sapphire_block", 1, 3.0f, 3.0f);
+    public static final Block AMETHYST_BLOCK = newOreBlock("amethyst_block", 1, 3.0f, 3.0f);
+    public static final Block AMBER_BLOCK = newOreBlock("amber_block", 1, 3.0f, 3.0f);
+    public static final Block PEARL_BLOCK = newOreBlock("pearl_block", 1, 3.0f, 3.0f);
+    public static final Block AQUAMARINE_BLOCK = newOreBlock("aquamarine_block", 1, 3.0f, 3.0f);
+    public static final Block ALEXANDRITE_BLOCK = newOreBlock("alexandrite_block", 1, 3.0f, 3.0f);
+
+    //植物、穀物ブロック
+    public static final Block COTTON_BLOCK = newBlock("cotton_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block TEA_LEAF_BLOCK = newBlock("tea_leaf_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block PAPPER_BLOCK = newBlock("papper_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block DING_BLOCK = newBlock("ding_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block CINAMMON_BLOCK = newBlock("cinammon_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block NATMEG_BLOCK = newBlock("natmeg_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block CARDAMON_BLOCK = newBlock("cardamon_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block GINGER_BLOCK = newBlock("ginger_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block OPIUM_BLOCK = newBlock("opium_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block RICE_BLOCK = newBlock("rice_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block SOY_BLOCK = newBlock("soy_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block WHEAT_BLOCK = newBlock("wheat_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
+    public static final Block CORN_BLOCK = newBlock("corn_block", Material.PLANTS, SoundType.PLANT, 1.0f, 2.0f);
 
     //蒸気機関、動力関係
     public static final Block SHAFT = new ShaftBlock(Block.Properties.create(Material.IRON).hardnessAndResistance(2.5f, 2.5f)).setRegistryName(EpochTechnologies.MODID, "shaft");
@@ -34,6 +61,32 @@ public class ETBlocks {
         registryBlock(r, SILVER_ORE);
         registryBlock(r, RUBY_ORE);
         registryBlock(r, SAPPHIRE_ORE);
+        registryBlock(r, COPPER_BLOCK);
+        registryBlock(r, TIN_BLOCK);
+        registryBlock(r, LEAD_BLOCK);
+        registryBlock(r, NICKEL_BLOCK);
+        registryBlock(r, SILVER_BLOCK);
+        registryBlock(r, RUBY_BLOCK);
+        registryBlock(r, SAPPHIRE_BLOCK);
+        registryBlock(r, AMETHYST_BLOCK);
+        registryBlock(r, AMBER_BLOCK);
+        registryBlock(r, PEARL_BLOCK);
+        registryBlock(r, AQUAMARINE_BLOCK);
+        registryBlock(r, ALEXANDRITE_BLOCK);
+
+        registryBlock(r, COTTON_BLOCK);
+        registryBlock(r, TEA_LEAF_BLOCK);
+        registryBlock(r, PAPPER_BLOCK);
+        registryBlock(r, DING_BLOCK);
+        registryBlock(r, CINAMMON_BLOCK);
+        registryBlock(r, NATMEG_BLOCK);
+        registryBlock(r, CARDAMON_BLOCK);
+        registryBlock(r, GINGER_BLOCK);
+        registryBlock(r, OPIUM_BLOCK);
+        registryBlock(r, RICE_BLOCK);
+        registryBlock(r, SOY_BLOCK);
+        registryBlock(r, WHEAT_BLOCK);
+        registryBlock(r, CORN_BLOCK);
 
         registryBlock(r, SHAFT);
 
@@ -49,6 +102,33 @@ public class ETBlocks {
         registryBlockItem(r, SILVER_ORE);
         registryBlockItem(r, RUBY_ORE);
         registryBlockItem(r, SAPPHIRE_ORE);
+        registryBlockItem(r, COPPER_BLOCK);
+        registryBlockItem(r, TIN_BLOCK);
+        registryBlockItem(r, LEAD_BLOCK);
+        registryBlockItem(r, NICKEL_BLOCK);
+        registryBlockItem(r, SILVER_BLOCK);
+        registryBlockItem(r, RUBY_BLOCK);
+        registryBlockItem(r, SAPPHIRE_BLOCK);
+        registryBlockItem(r, AMETHYST_BLOCK);
+        registryBlockItem(r, AMBER_BLOCK);
+        registryBlockItem(r, PEARL_BLOCK);
+        registryBlockItem(r, AQUAMARINE_BLOCK);
+        registryBlockItem(r, ALEXANDRITE_BLOCK);
+
+        registryBlockItem(r, COTTON_BLOCK);
+        registryBlockItem(r, TEA_LEAF_BLOCK);
+        registryBlockItem(r, PAPPER_BLOCK);
+        registryBlockItem(r, DING_BLOCK);
+        registryBlockItem(r, CINAMMON_BLOCK);
+        registryBlockItem(r, NATMEG_BLOCK);
+        registryBlockItem(r, CARDAMON_BLOCK);
+        registryBlockItem(r, GINGER_BLOCK);
+        registryBlockItem(r, OPIUM_BLOCK);
+        registryBlockItem(r, RICE_BLOCK);
+        registryBlockItem(r, SOY_BLOCK);
+        registryBlockItem(r, WHEAT_BLOCK);
+        registryBlockItem(r, CORN_BLOCK);
+
 
         registryBlockItem(r, SHAFT);
 
--- src/main/java/red/felnull/epochtechnologies/item/ETItems.java
+++ src/main/java/red/felnull/epochtechnologies/item/ETItems.java
@@ -8,34 +8,6 @@ public class ETItems {
     public static final Item TEST_ITEM = new TestItem(new Item.Properties().group(ETItemGroup.MOD_TAB))
             .setRegistryName(EpochTechnologies.MODID, "test_item");
 
-    //鉱石
-    public static final Item copper_ore = newItem("copper_ore");
-    public static final Item tin_ore = newItem("tin_ore");
-    public static final Item lead_ore = newItem("lead_ore");
-    public static final Item nickel_ore = newItem("nickel_ore");
-    public static final Item silver_ore = newItem("silver_ore");
-    public static final Item ruby_ore = newItem("ruby_ore");
-    public static final Item sapphire_ore = newItem("sapphire_ore");
-    public static final Item amethyst_ore = newItem("amethyst_ore");
-    public static final Item amber_ore = newItem("amber_ore");
-    public static final Item pearl_ore = newItem("pearl_ore");
-    public static final Item aquamarine_ore = newItem("aquamarine_ore");
-    public static final Item alexandrite_ore = newItem("alexandrite_ore");
-
-    //鉱石ブロック
-    public static final Item copper_block = newItem("copper_block");
-    public static final Item tin_block = newItem("tin_block");
-    public static final Item lead_block = newItem("lead_block");
-    public static final Item nickel_block = newItem("nickel_block");
-    public static final Item silver_block = newItem("silver_block");
-    public static final Item ruby_block = newItem("ruby_block");
-    public static final Item sapphire_block = newItem("sapphire_block");
-    public static final Item amethyst_block = newItem("amethyst_block");
-    public static final Item amber_block = newItem("amber_block");
-    public static final Item pearl_block = newItem("pearl_block");
-    public static final Item aquamarine_block = newItem("aquamarine_block");
-    public static final Item alexandrite_block = newItem("alexandrite_block");
-
     //インゴット
     public static final Item COPPER_INGOT = newItem("copper_ingot");
     public static final Item TIN_INGOT = newItem("tin_ingot");
@@ -46,105 +18,64 @@ public class ETItems {
     //宝石
     public static final Item RUBY = newItem("ruby");
     public static final Item SAPPHIRE = newItem("sapphire");
-    public static final Item amethyst = newItem("amethyst");
-    public static final Item amber = newItem("amber");
-    public static final Item pearl = newItem("pearl");
-    public static final Item aquamarine = newItem("aquamarine");
-    public static final Item alexandrite = newItem("alexandrite");
+    public static final Item AMETHYST = newItem("amethyst");
+    public static final Item AMBER = newItem("amber");
+    public static final Item PEARL = newItem("pearl");
+    public static final Item AQUAMARINE = newItem("aquamarine");
+    public static final Item ALEXANDRITE = newItem("alexandrite");
 
     //種
-    public static final Item cotton_seeds = newItem("cotton_seeds");
-    public static final Item tea_leaf_seeds = newItem("tea_leaf_seeds");
-    public static final Item papper_seeds = newItem("papper_seeds");
-    public static final Item ding_seeds = newItem("ding_seeds");
-    public static final Item cinammon_seeds = newItem("cinammon_seeds");
-    public static final Item natmeg_seeds = newItem("natmeg_seeds");
-    public static final Item cardamon_seeds = newItem("cardamon_seeds");
-    public static final Item ginger_seeds = newItem("ginger_seeds");
-    public static final Item opium_seeds = newItem("opium_seeds");
-    public static final Item rice_seeds = newItem("rice_seeds");
-    public static final Item soy_seeds = newItem("soy_seeds");
-    public static final Item wheat_seeds = newItem("wheat_seeds");
-    public static final Item corn_seeds = newItem("corn_seeds");
-
-    //植物、穀物ブロック
-    public static final Item cotton_block = newItem("cotton_block");
-    public static final Item tea_leaf_block = newItem("tea_leaf_block");
-    public static final Item papper_block = newItem("papper_block");
-    public static final Item ding_block = newItem("ding_block");
-    public static final Item cinammon_block = newItem("cinammon_block");
-    public static final Item natmeg_block = newItem("natmeg_block");
-    public static final Item cardamon_block = newItem("cardamon_block");
-    public static final Item ginger_block = newItem("ginger_block");
-    public static final Item opium_block = newItem("opium_block");
-    public static final Item rice_block = newItem("rice_block");
-    public static final Item soy_block = newItem("soy_block");
-    public static final Item wheat_block = newItem("wheat_block");
-    public static final Item corn_block = newItem("corn_block");
+    public static final Item COTTON_SEEDS = newItem("cotton_seeds");
+    public static final Item TEA_LEAF_SEEDS = newItem("tea_leaf_seeds");
+    public static final Item PAPPER_SEEDS = newItem("papper_seeds");
+    public static final Item DING_SEEDS = newItem("ding_seeds");
+    public static final Item CINAMMON_SEEDS = newItem("cinammon_seeds");
+    public static final Item NATMEG_SEEDS = newItem("natmeg_seeds");
+    public static final Item CARDAMON_SEEDS = newItem("cardamon_seeds");
+    public static final Item GINGER_SEEDS = newItem("ginger_seeds");
+    public static final Item OPIUM_SEEDS = newItem("opium_seeds");
+    public static final Item RICE_SEEDS = newItem("rice_seeds");
+    public static final Item SOY_SEEDS = newItem("soy_seeds");
+    public static final Item WHEAT_SEEDS = newItem("wheat_seeds");
+    public static final Item CORN_SEEDS = newItem("corn_seeds");
 
     //植物
     public static final Item COTTON = newItem("cotton");
     public static final Item TEA_LEAF = newItem("tea_leaf");
-    public static final Item papper = newItem("papper");
-    public static final Item ding = newItem("ding");
-    public static final Item cinammon = newItem("cinammon");
-    public static final Item natmeg = newItem("natmeg");
-    public static final Item cardamom = newItem("cardamon");
-    public static final Item ginger = newItem("ginger");
-    public static final Item opium = newItem("opium");
+    public static final Item PAPPER = newItem("papper");
+    public static final Item DING = newItem("ding");
+    public static final Item CINAMMON = newItem("cinammon");
+    public static final Item NATMEG = newItem("natmeg");
+    public static final Item CARDAMOM = newItem("cardamon");
+    public static final Item GINGER = newItem("ginger");
+    public static final Item OPIUM = newItem("opium");
 
     //穀物
-    public static final Item rice = newItem("rice");
-    public static final Item soy = newItem("soy");
-    public static final Item wheat = newItem("wheat");
-    public static final Item corn = newItem("corn");
+    public static final Item RICE = newItem("rice");
+    public static final Item SOY = newItem("soy");
+    public static final Item WHEAT = newItem("wheat");
+    public static final Item CORN = newItem("corn");
 
     //毛皮
-    public static final Item wolfhide = newItem("wolfhide");
-    public static final Item sheephide = newItem("sheephide");
-    public static final Item pighide = newItem("pighide");
-    public static final Item foxhide = newItem("foxhide");
-    public static final Item polar_bearhide = newItem("polar_bearhide");
-    public static final Item pandahide = newItem("pandahide");
-    public static final Item hoglinhide = newItem("hoglinhide");
-    public static final Item dolphinhide = newItem("dolphinhide");
-    public static final Item cathide = newItem("cathide");
-    public static final Item turtlehide = newItem("turtlehide");
-    public static final Item bathide = newItem("bathide");
-    public static final Item horsehide = newItem("horsehide");
-    public static final Item humanhide = newItem("humanhide");
-    public static final Item ravegerhide = newItem("ravegerhide");
-
+    public static final Item WOLFHIDE = newItem("wolfhide");
+    public static final Item SHEEPHIDE = newItem("sheephide");
+    public static final Item PIGHIDE = newItem("pighide");
+    public static final Item FOXHIDE = newItem("foxhide");
+    public static final Item POLAR_BEARHIDE = newItem("polar_bearhide");
+    public static final Item PANDAHIDE = newItem("pandahide");
+    public static final Item HOGLINHIDE = newItem("hoglinhide");
+    public static final Item DOLPHINHIDE = newItem("dolphinhide");
+    public static final Item CATHIDE = newItem("cathide");
+    public static final Item TURTLEHIDE = newItem("turtlehide");
+    public static final Item BATHIDE = newItem("bathide");
+    public static final Item HORSEHIDE = newItem("horsehide");
+    public static final Item HUMANHIDE = newItem("humanhide");
+    public static final Item RAVEGERHIDE = newItem("ravegerhide");
 
 
     public static void registerItem(IForgeRegistry<Item> r) {
+
         registryItem(r, TEST_ITEM);
-
-        registryItem(r,copper_ore );
-        registryItem(r,tin_ore );
-        registryItem(r,lead_ore );
-        registryItem(r,nickel_ore );
-        registryItem(r,silver_ore );
-        registryItem(r,ruby_ore );
-        registryItem(r,sapphire_ore );
-        registryItem(r,amethyst_ore );
-        registryItem(r,amber_ore );
-        registryItem(r,pearl_ore );
-        registryItem(r,aquamarine_ore );
-        registryItem(r,alexandrite_ore );
-
-        registryItem(r,copper_block );
-        registryItem(r,tin_block );
-        registryItem(r,lead_block );
-        registryItem(r,nickel_block );
-        registryItem(r,silver_block );
-        registryItem(r,ruby_block );
-        registryItem(r,sapphire_block );
-        registryItem(r,amethyst_block );
-        registryItem(r,amber_block );
-        registryItem(r,pearl_block );
-        registryItem(r,aquamarine_block );
-        registryItem(r,alexandrite_block );
 
         registryItem(r, COPPER_INGOT);
         registryItem(r, TIN_INGOT);
@@ -154,69 +85,56 @@ public class ETItems {
 
         registryItem(r, RUBY);
         registryItem(r, SAPPHIRE);
-        registryItem(r,amethyst );
-        registryItem(r,amber );
-        registryItem(r,pearl );
-        registryItem(r,aquamarine );
-        registryItem(r,alexandrite );
+        registryItem(r, AMETHYST);
+        registryItem(r, AMBER);
+        registryItem(r, PEARL);
+        registryItem(r, AQUAMARINE);
+        registryItem(r, ALEXANDRITE);
 
-        registryItem(r,cotton_seeds );
-        registryItem(r,tea_leaf_seeds );
-        registryItem(r,papper_seeds );
-        registryItem(r,ding_seeds );
-        registryItem(r,cinammon_seeds );
-        registryItem(r,natmeg_seeds );
-        registryItem(r,cardamon_seeds );
-        registryItem(r,ginger_seeds );
-        registryItem(r,opium_seeds );
-        registryItem(r,rice_seeds );
-        registryItem(r,soy_seeds );
-        registryItem(r,wheat_seeds );
-        registryItem(r,corn_seeds );
-
-        registryItem(r,cotton_block );
-        registryItem(r,tea_leaf_block );
-        registryItem(r,papper_block );
-        registryItem(r,ding_block );
-        registryItem(r,cinammon_block );
-        registryItem(r,natmeg_block );
-        registryItem(r,cardamon_block );
-        registryItem(r,ginger_block );
-        registryItem(r,opium_block );
-        registryItem(r,rice_block );
-        registryItem(r,soy_block );
-        registryItem(r,wheat_block );
-        registryItem(r,corn_block );
+        registryItem(r, COTTON_SEEDS);
+        registryItem(r, TEA_LEAF_SEEDS);
+        registryItem(r, PAPPER_SEEDS);
+        registryItem(r, DING_SEEDS);
+        registryItem(r, CINAMMON_SEEDS);
+        registryItem(r, NATMEG_SEEDS);
+        registryItem(r, CARDAMON_SEEDS);
+        registryItem(r, GINGER_SEEDS);
+        registryItem(r, OPIUM_SEEDS);
+        registryItem(r, RICE_SEEDS);
+        registryItem(r, SOY_SEEDS);
+        registryItem(r, WHEAT_SEEDS);
+        registryItem(r, CORN_SEEDS);
 
         registryItem(r, COTTON);
         registryItem(r, TEA_LEAF);
-        registryItem(r, papper);
-        registryItem(r, ding);
-        registryItem(r, cinammon);
-        registryItem(r, natmeg);
-        registryItem(r, cardamom);
-        registryItem(r, ginger);
-        registryItem(r, opium);
+        registryItem(r, PAPPER);
+        registryItem(r, DING);
+        registryItem(r, CINAMMON);
+        registryItem(r, NATMEG);
+        registryItem(r, CARDAMOM);
+        registryItem(r, GINGER);
+        registryItem(r, OPIUM);
 
-        registryItem(r,rice );
-        registryItem(r,soy );
-        registryItem(r,wheat );
-        registryItem(r,corn );
+        registryItem(r, RICE);
+        registryItem(r, SOY);
+        registryItem(r, WHEAT);
+        registryItem(r, CORN);
 
-        registryItem(r,wolfhide );
-        registryItem(r,sheephide );
-        registryItem(r,pighide );
-        registryItem(r,foxhide );
-        registryItem(r,polar_bearhide );
-        registryItem(r,pandahide );
-        registryItem(r,hoglinhide );
-        registryItem(r,dolphinhide );
-        registryItem(r,cathide );
-        registryItem(r,turtlehide );
-        registryItem(r,bathide );
-        registryItem(r,horsehide );
-        registryItem(r,humanhide );
-        registryItem(r,ravegerhide );
+        registryItem(r, WOLFHIDE);
+        registryItem(r, SHEEPHIDE);
+        registryItem(r, PIGHIDE);
+        registryItem(r, FOXHIDE);
+        registryItem(r, POLAR_BEARHIDE);
+        registryItem(r, PANDAHIDE);
+        registryItem(r, HOGLINHIDE);
+        registryItem(r, DOLPHINHIDE);
+        registryItem(r, CATHIDE);
+        registryItem(r, TURTLEHIDE);
+        registryItem(r, BATHIDE);
+        registryItem(r, HORSEHIDE);
+        registryItem(r, HUMANHIDE);
+        registryItem(r, RAVEGERHIDE);
+
 
     }
 
